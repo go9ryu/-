6c7520c67d24c2c5248efb82d21cb45792fdadf7
go9ryu <go9ryu@gmail.com>
2026-09-13 23:16:44 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,10 @@ from pydantic import BaseModel, Field
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+@app.post("/api/save")
+def save_report(data: BookReportSchema):
+    return {"message": "success"}
+    
 app = FastAPI()
 
 app.add_middleware(
